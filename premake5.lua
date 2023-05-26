e9a5aef716df7dc38e33334711774872b23ef7c8
RaphaelIT7 <raphael.i.t@outlook.de>
2023-05-26 15:22:09 +0200

    Fixed potential errors for linux compile
    This will fix some errors that could occur in the future.
    This error occurred in some of my projects.

--- premake5.lua
+++ premake5.lua
@@ -11,6 +11,7 @@ newoption({
 local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
     "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
+IncludeHelpersExtended()
 
 CreateWorkspace({name = "cache", abi_compatible = false})
     -- Serverside module (gmsv prefix)
@@ -38,3 +39,9 @@ CreateWorkspace({name = "cache", abi_compatible = false})
 
         filter("system:linux or macosx")
             files({"source/posix/*.cpp", "source/posix/*.hpp"})
+                filter "system:linux"
+
+                links // this fixes the undefined reference to `dlopen' errors.
+                {
+                    "dl",
+                }
